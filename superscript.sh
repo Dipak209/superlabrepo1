cb5b72676d9ff9284b81e7f4fe2bbde5ba0db0f6
dipak <dipak@gmail.com>
2025-07-26 09:25:05 +0000

    Intial comment

---
+++ superscript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+echo "System Report" > system_report.txt 
+echo "Generated on: $(date)" >> system_report.txt 
+echo "" >> system_report.txt 
+echo "Logged-in Users:" >> system_report.txt 
+who >> system_report.txt 
+echo "" >> system_report.txt 
+echo "System Uptime:" >> system_report.txt 
+uptime >> system_report.txt 
+echo "System report generated: system_report.txt"
+
